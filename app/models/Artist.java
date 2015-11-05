0f48191b92508c3fb1cf171cab4c8b6ebb39b406
jonatan <j.pineda.p1@gmail.com>
2015-11-04 18:55:44 -0700

    Se completan los modelos

--- app/models/Artist.java
+++ app/models/Artist.java
@@ -1,14 +1,35 @@
 package models;
 
 import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Reference;
 import play.db.jpa.Model;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author arturo
+ *
+ * This class represents an artist who uploads his songs.
+ *
  */
 
 @Entity
 public class Artist extends Model {
-    
+
+    public File artistImage;
+    public String artistName;
+    public String artistDescription;
+
+    @Reference
+    public List<Song> artistSongs;
+
+    public Artist(File artistImage, String artistName, String artistDescription) {
+        this.artistImage = artistImage;
+        this.artistName = artistName;
+        this.artistDescription = artistDescription;
+        this.artistSongs = new ArrayList<Song>();
+    }
 }
--- app/models/Song.java
+++ app/models/Song.java
@@ -1,12 +1,30 @@
 package models;
 
 import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Reference;
 import play.db.jpa.Model;
+
+import java.io.File;
 
 /**
  * Created by Jonatan on 04/11/2015.
+ *
+ * This class represents a song of an Artist.
  */
 
 @Entity
 public class Song extends Model {
+
+    public String songName;
+    public File audio;
+
+    @Reference
+    public Artist author;
+
+    public Song(String songName, File audio, Artist author) {
+        this.songName = songName;
+        this.audio = audio;
+        this.author = author;
+    }
+
 }
