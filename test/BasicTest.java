3a733573d1adc0b9703d8bedcdb02efbb68a4cf3
jonatan <j.pineda.p1@gmail.com>
2015-11-23 17:51:13 -0700

    se implementaron pruebas unitarias y reproductor

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -10,4 +10,49 @@ public class BasicTest extends UnitTest {
         assertEquals(2, 1 + 1);
     }
 
+    @Test
+    public void createAndRetrieveArtist() {
+        new Artist(null, "Miles", "miles@gmail.com", "secret",
+            "Cool Jazz for the people").save();
+
+        Artist miles = Artist.find("byEmail", "miles@gmail.com").first();
+
+        //Test
+        assertNotNull(miles);
+        assertEquals("Miles", miles.artistName);
+    }
+
+    @Test
+    public void uploadSong() {
+        new Artist(null, "Miles", "miles@gmail.com", "secret",
+                "Cool Jazz for the people").save();
+
+        Artist miles = Artist.find("byEmail", "miles@gmail.com").first();
+
+        Song newSong =  new Song("Bitches Brew", null, miles);
+
+        assertNotNull(newSong);
+        assertEquals("Bitches Brew", newSong.songName);
+
+    }
+
+    @Test
+    public void getSongsAndExist() {
+        List<Song> songs = Song.q().asList();
+
+        assertNotNull(songs);
+    }
+
+    @Test
+    public void tryConnectAsArtist() {
+        // Create a new Artist and save it
+        new Artist(null, "Miles", "miles@gmail.com", "secret",
+                "Cool Jazz for the people").save();
+
+        // Test
+        assertNotNull(Artist.connect("miles@gmail.com", "secret"));
+        assertNull(Artist.connect("miles@gmail.com", "badpassword"));
+        assertNull(Artist.connect("miles@gmail.com", "secret"));
+    }
+
 }
